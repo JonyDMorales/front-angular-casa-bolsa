fe4dac1a4452d9e98e4057e87fbb30cef3e4aeec
Morales Torres Jonatan <xme1882@XME1882MAC.local>
2020-03-17 21:56:43 -0600

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LimitesComponent } from './components/limites/limites.component';
+import { VarHistoricoComponent } from './components/var-historico/var-historico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LimitesComponent,
+    VarHistoricoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/var-historico/var-historico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-var-historico',
+  templateUrl: './var-historico.component.html',
+  styleUrls: ['./var-historico.component.css']
+})
+export class VarHistoricoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

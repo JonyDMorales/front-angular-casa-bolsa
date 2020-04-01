a0b16bd11d179e5e42651a7190711e06c671591d
Angel <angel.abel426@gmail.com>
2020-03-31 22:45:11 -0600

    Limites lineas js

--- src/app/components/limites/limites.component.ts
+++ src/app/components/limites/limites.component.ts
@@ -23,7 +23,11 @@ export class LimitesComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.cambioDivisasMethod('contraparte');
+
+
+    this.funcionUno()
+
+   // this.cambioDivisasMethod('contraparte');
   }
 
 
@@ -613,6 +617,80 @@ export class LimitesComponent implements OnInit {
     }
 
 
+  }
+
+
+
+
+
+
+
+
+///// Metodo con appends //////
+
+
+  public funcionUno() {
+    this.backendService.limites(0).subscribe(da => {
+      $('#conteTable').empty();
+      const divisaValor = 1;
+      $('#conteTable').append('<table class="table table-striped" id="limites" >' +
+        '<thead>' +
+        '<tr>' +
+        '<th>contraparte</th>' +
+        '<th>Limite Global</th>' +
+        '<th>Límite Operaciones Directo</th>' +
+        '<th>Límite Operaciones en Reporto</th>' +
+        '<th>Límite por Operción</th>' +
+        '<th>Límite Mercado de Cambios</th>' +
+        '<th>Límite por Operción Mercado de Cambios</th>' +
+        '<th></th>' +
+        '</tr>' +
+        '</thead>' +
+        '<tbody id="tableLimites">' +
+
+        '</tbody>' +
+        '</table>');
+      for (var i = 0; i < da['length']; i++) {
+
+        const globalLimitConverted = ((parseFloat(da[i]['globalLimit'])) * divisaValor) / 1;
+
+        const directOperationLimit = ((parseFloat(da[i]['directOperationLimit'])) * divisaValor) / 1;
+        const reportoOperationLimit = ((parseFloat(da[i]['reportoOperationLimit'])) * divisaValor) / 1;
+        const operationLimitMoneyMarket = ((parseFloat(da[i]['operationLimitMoneyMarket'])) * divisaValor) / 1;
+        const exchangeMarketLimit = ((parseFloat(da[i]['exchangeMarketLimit'])) * divisaValor) / 1;
+        const limitOperationExchangeMarket = ((parseFloat(da[i]['limitOperationExchangeMarket'])) * divisaValor) / 1;
+
+
+        $('#tableLimites').append('<tr  id="' + da[i]['contraparte'] + '">' +
+          '<td>' + da[i]['contraparte'] + '</td>' +
+
+          '<td>' + globalLimitConverted.toFixed(2) + '</td>' +
+          '<td>' + directOperationLimit.toFixed(2) + '</td>' +
+          '<td>' + reportoOperationLimit.toFixed(2) + '</td>' +
+          '<td>' + operationLimitMoneyMarket.toFixed(2) + '</td>' +
+          '<td>' + exchangeMarketLimit.toFixed(2) + '</td>' +
+          '<td>' + limitOperationExchangeMarket.toFixed(2) + '</td>' +
+
+
+          "<td><a class=\"btn btn-danger btn-xs\" style=\"color: white\" (click)=\"eliminar()\">Eliminar</a></td>"+
+
+          '</tr>');
+
+
+
+      }
+    });
+
+
+
+
+  }
+
+  public eliminar(){
+
+
+    console.log("Entro en el metodo")
+      
   }
 
 
